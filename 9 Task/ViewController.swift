d96a627f6748e6140ba4ac624b46330dee5d36a7
Ruslan Gaifullin <gayfullin.r.i@yandex.ru>
2023-05-25 21:11:31 +0300

    init commit

--- 9 Task/ViewController.swift
+++ 9 Task/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 20
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 20, right: 10)
+
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.backgroundColor = .systemBackground
+        collection.delegate = self
+        collection.dataSource = self
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.showsHorizontalScrollIndicator = false
+        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "collectionCell")
+        return collection
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Colllection"
+        view.addSubview(collectionView)
+        setupConstraits()
+        
+        }
+    
+    private func setupConstraits() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+        ])
     }
-
-
 }
 
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = view.frame.width - 40
+        let height: CGFloat = collectionView.frame.height / 1.5
+        return CGSize(width: width, height: height)
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint> ){
+           let pageWidth: CGFloat = view.frame.width - 40
+           let cellWidth = pageWidth + 20
+           let targetContentOffsetXPoint = targetContentOffset.pointee.x / cellWidth
+           targetContentOffset.pointee.x = round(targetContentOffsetXPoint) * cellWidth - 20
+       }
+   }
+
+extension ViewController: UICollectionViewDataSource {
+       func collectionView( _ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           10
+       }
+
+       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath)
+           cell.backgroundColor = UIColor.init(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1), alpha: 1)
+           cell.layer.cornerRadius = 10
+           return cell
+       }
+   }
